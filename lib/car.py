df27a978f11709e128d563aee48e55eedc9dfdb2
Emmanuel Mutugi <smontana025@gmail.com>
2024-12-13 14:53:20 +0300

    mannuh

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,8 @@
-from vehicle import Vehicle
+from vehicle import Vehicle  # Import Vehicle class
 
-class Car(Vehicle):
-    pass
+class Car(Vehicle):  # Inherit from Vehicle
+    def __init__(self, wheel_size, wheel_number):  # Accept parameters in __init__
+        super().__init__(wheel_size, wheel_number)  # Call the parent class constructor
+
+    def go(self):  # Override go() method
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
